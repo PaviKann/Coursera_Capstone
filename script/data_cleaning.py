5c716470d5056af1e817ff0b5e10e7fe7519b1d6
PaviKann <PaviKann@users.noreply.github.com>
2020-10-20 12:33:58 +0530

    Delete data_cleaning.py

--- script/data_cleaning.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as ny
-from IPython.display import display
-
-
-
-df = pd.read_csv('~/Documents/Capstone/Data-Collisions.csv')
-
-display(df["WEATHER"].value_counts())
